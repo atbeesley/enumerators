4aa6ed05ca15830df2fbcbebea0f9c624ab87c61
user <you@exampalex.com>
2019-10-16 16:08:15 +0100

    initial commit

---
+++ models/traveller.js
@@ -0,0 +1,43 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === "train";
+  })
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance >= 1000
+  })
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance
+  }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+return this.journeys.map((journey) => {
+  return journey.transport;
+})
+
+};
+
+
+module.exports = Traveller;
